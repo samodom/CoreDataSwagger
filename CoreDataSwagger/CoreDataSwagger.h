993e79ee79fda5f42a94deac296849ed4d00515b
Sam Odom <samodom@me.com>
2014-10-12 17:46:32 -0500

    Initial Commit

---
+++ CoreDataSwagger/CoreDataSwagger.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataSwagger.h
+//  CoreDataSwagger
+//
+//  Created by Sam Odom on 10/12/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataSwagger.
+FOUNDATION_EXPORT double CoreDataSwaggerVersionNumber;
+
+//! Project version string for CoreDataSwagger.
+FOUNDATION_EXPORT const unsigned char CoreDataSwaggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataSwagger/PublicHeader.h>
+
+
