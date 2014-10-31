766dd1367af8eb2cbbd2768c071cc9b183adcd72
Sam Odom <samodom@me.com>
2014-10-30 19:29:00 -0500

    Updated stack creation

--- CoreDataSwagger/CoreDataStack.swift
+++ CoreDataSwagger/CoreDataStack.swift
@@ -11,31 +11,23 @@ import CoreData
 public typealias CoreDataPerformClosure = () -> Void
 
 public class CoreDataStack {
-    public let model: NSManagedObjectModel
-    public let coordinator: NSPersistentStoreCoordinator
-    public let rootContext: NSManagedObjectContext
 
-    public init(datastoreURL: NSURL) {
-        model = NSManagedObjectModel.mergedModelFromBundles(NSBundle.allBundles()) ?? NSManagedObjectModel()
-        coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
-        var error: NSError?
-        coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: datastoreURL, options: nil, error: &error)
-        if error != nil {
-            NSLog("Error adding persistent store to coordinator (URL: \(datastoreURL.absoluteString!): \(error!.debugDescription)")
-        }
-        rootContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-        rootContext.persistentStoreCoordinator = coordinator
-    }
+    public let model: NSManagedObjectModel!
+    public let coordinator: NSPersistentStoreCoordinator!
+    public let rootContext: NSManagedObjectContext!
 
-    public init() {
-        model = NSManagedObjectModel.mergedModelFromBundles(NSBundle.allBundles()) ?? NSManagedObjectModel()
-        coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
-        var error: NSError?
-        coordinator.addPersistentStoreWithType(NSInMemoryStoreType, configuration: nil, URL: nil, options: nil, error: &error)
-        if error != nil {
-            NSLog("Error adding in-memory store to coordinator: \(error!.debugDescription)")
+    public init?(configuration: CoreDataStackConfiguration = CoreDataStackConfiguration()) {
+        if let modelFromSource = NSManagedObjectModel.createFromSource(configuration.modelSource) {
+            model = modelFromSource
         }
-        rootContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        else {
+            NSLog("Error creating managed object model")
+            return nil
+        }
+
+        coordinator = NSPersistentStoreCoordinator.createWithModel(model, storeParameters: configuration.storeParameters)
+
+        rootContext = NSManagedObjectContext(concurrencyType: configuration.contextConcurrencyType)
         rootContext.persistentStoreCoordinator = coordinator
     }
 
---
+++ CoreDataSwaggerTests/Tests/CoreDataStackCreationTests.swift
@@ -0,0 +1,63 @@
+//
+//  CoreDataStackCreationTests.swift
+//  CoreDataSwagger
+//
+//  Created by Sam Odom on 10/12/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+import CoreData
+import XCTest
+
+class CoreDataStackCreationTests: XCTestCase {
+
+    var stack: CoreDataStack!
+    var storeURL: NSURL!
+
+    override func setUp() {
+        super.setUp()
+
+        storeURL = URLofFileInDocumentsDirectory(named: "datastore.sqlite")!
+        DeleteFile(atURL: storeURL)
+    }
+    
+    override func tearDown() {
+        DeleteFile(atURL: storeURL)
+
+        super.tearDown()
+    }
+
+    func testDefaultStackCreation() {
+        stack = CoreDataStack()
+        XCTAssertTrue(stack != nil, "A stack should be created with the default configuration")
+        XCTAssertEqual(stack.model.entities.count, 0, "There are no models in the main bundle, so the entity count should be zero")
+        XCTAssertEqual(stack.coordinator.persistentStores.count, 1, "There should be one persistent store")
+        XCTAssertEqual(stack.rootContext.concurrencyType, NSManagedObjectContextConcurrencyType.PrivateQueueConcurrencyType, "The default context concurrency type should be the private queue type")
+        XCTAssertEqual(stack.rootContext.persistentStoreCoordinator!, stack.coordinator, "The context should use the stack's coordinator")
+    }
+
+    func createModelSource() -> CoreDataModelSource {
+        let modelOne = LoadModel(named: "SampleModelOne")
+        let modelTwo = LoadModel(named: "SampleModelTwo")
+        return CoreDataModelSource(models: [modelOne, modelTwo])
+    }
+
+    func createStoreParameters() -> [CoreDataStoreParameters] {
+        let parametersOne = CoreDataStoreParameters()
+        let parametersTwo = CoreDataStoreParameters.SQLite(URL: storeURL, configuration: nil, options: nil)
+        return [parametersOne, parametersTwo]
+    }
+
+    func testCustomStackCreationNoMetadata() {
+        let modelSource = createModelSource()
+        let storeParameters = createStoreParameters()
+        let configuration = CoreDataStackConfiguration(concurrency: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType, modelSource: modelSource, storeParameters: storeParameters)
+        stack = CoreDataStack(configuration: configuration)
+        XCTAssertTrue(stack != nil, "A stack should be created with the custom configuration")
+        XCTAssertEqual(stack.model.entities.count, 2, "There should be two total entities across all of the models")
+        XCTAssertEqual(stack.coordinator.persistentStores.count, 2, "There should be two total persistent stores created")
+        XCTAssertEqual(stack.rootContext.concurrencyType, NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType, "The stack should create a managed object context with the provided concurency type")
+        XCTAssertEqual(stack.rootContext.persistentStoreCoordinator!, stack.coordinator, "The context should use the stack's coordinator")
+    }
+
+}
